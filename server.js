a474d1a1fec474341e5baf752db6254933c4590c
fewieden <felix@wiedenba.ch>
2018-02-26 08:14:26 +0100

    added body parser

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
 const app = express();
 
+app.use(bodyParser.json());
 app.use('/', routes);
 
 app.all('*', (req, res) => res.send('Hello World!'));
